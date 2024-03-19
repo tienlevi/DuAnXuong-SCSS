98d4998692d6a846b316b0ff067ccd6fb096a66c
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-19 09:38:02 +0700

    fix route

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "./index.html"),
         product: resolve(__dirname, "./product.html"),
-        productdetail: resolve(__dirname, "./product-detail.html"),
+        productdetail: resolve(__dirname, "./product-detail.html.html"),
         cart: resolve(__dirname, "./cart.html"),
         checkout: resolve(__dirname, "./checkout.html"),
       },
